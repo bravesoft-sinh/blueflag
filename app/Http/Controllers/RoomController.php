7fed5d964065bc6cb880911658c8f5bbd4f22fbf
Hoang Ngoc Sinh <h.sinh@bravesoft.co.jp>
2021-01-18 02:26:55 +0900

    fix controller

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -11,7 +11,7 @@ class RoomController extends Controller
 {
     public function index()
     {
-        $rooms = DB::select('explain select u.name as user_name, r.name as room_name, ur.status as status from user u inner join user_room ur on u.id = ur.user_id
+        $rooms = DB::select('select u.name as user_name, r.name as room_name, ur.status as status from user u inner join user_room ur on u.id = ur.user_id
 inner join room r on ur.room_id = r.id');
         return response()->json([
             'code' => 200,
