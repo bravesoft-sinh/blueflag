5a22e2039e591128b57114f05bf073fbc0edaedd
Hoang Ngoc Sinh <h.sinh@bravesoft.co.jp>
2021-01-18 02:12:34 +0900

    add logic

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+
 
 class RoomController extends Controller
 {
-    //
+    public function index()
+    {
+        $rooms = DB::select(' select * from user u inner join user_room ur on u.id = ur.user_id inner join room r on ur.room_id = r.id');
+        return response()->json([
+            'code' => 200,
+            'message' => 'success',
+            'data' => [
+                'room' => $rooms
+            ]
+        ], CODE_SUCCESS);
+
+
+    }
 }
