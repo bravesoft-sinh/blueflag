bfe4ef52c47f92afec79dc6e0372c51e42039d61
Hoang Ngoc Sinh <h.sinh@bravesoft.co.jp>
2021-01-18 02:18:51 +0900

    fix controller

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/rooms', 'RoomController@index');
+Route::get('/rooms', 'App\Http\Controllers\RoomController@index');
